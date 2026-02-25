cdff680d830f05ddbeae6e136418def329e5b3a3
aparna-workspace <aparnasworkspace@gmail.com>
2026-02-25 13:21:56 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// supabase-config.js
+// IMPORTANT: paste your own values
+const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL";
+const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
