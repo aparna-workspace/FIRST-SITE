767bfaa152f70e4558733ad95d1eb5f9a4c2c9a5
aparna-workspace <aparnasworkspace@gmail.com>
2026-02-25 13:30:07 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // supabase-config.js
 // IMPORTANT: paste your own values
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL";
-const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY";
+const SUPABASE_URL = "https://tmvhocwdlqrmakfofbir.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vS8Tj6QJp3jkh5pxgEM8kw_2qFhdNS0";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
